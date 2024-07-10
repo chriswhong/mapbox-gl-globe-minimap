95521c29d436b51a5afe7873b7f148057504af5e
Chris Whong <chris.m.whong@gmail.com>
2024-07-09 22:33:28 -0400

    add and run prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,17 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {languageOptions: { globals: {
-    ...globals.browser,
-    mapboxgl: true
-  } }},
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        mapboxgl: true,
+      },
+    },
+  },
+  {
+    ignores: ["dist/*"],
+  },
   pluginJs.configs.recommended,
 ];
